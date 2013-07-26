c9ca0ac8e4ae09abb718491db5131d5efe6ea5a0
naydoummar <doummar@adobe.com>
2013-07-26 10:23:01 -0700

    Update add_metrics.rb

--- script/add_metrics.rb
+++ script/add_metrics.rb
@@ -4,7 +4,7 @@ require 'rubygems'
 require 'yaml'
 require 'aws-sdk'
 require 'aws/cloud_watch'
-require '.functions.rb'
+require './functions.rb'
 
 #Parameters
 nodes_subscribed=get_string_from_file($cw_metrics_values_file,'nodes_subscribed = ')
@@ -13,6 +13,7 @@ nodes_active= get_string_from_file($cw_metrics_values_file,'nodes_active = ')
 #Access Key and Secret Key of cloudwatch IAM User
 cw_access_key,cw_secret_key = get_aws_credentials($initial_config_file,'cw_accesskey = ','cw_secretkey = ')
 #Metrics- namespace, name
+cw_region = get_string_from_file($initial_config_file,'cw_region = ')
 namespace = get_string_from_file($initial_config_file,'namespace = ')
 metric1 = get_string_from_file($initial_config_file,'metric1 = ')
 metric2 = get_string_from_file($initial_config_file,'metric2 = ')
@@ -22,7 +23,7 @@ metric3 = get_string_from_file($initial_config_file,'metric3 = ')
 AWS.config(
         :access_key_id => cw_access_key,
         :secret_access_key => cw_secret_key,
-        :region => 'us-east-1'
+        :region => cw_region
         )
 
 @cw = AWS::CloudWatch.new
@@ -45,4 +46,3 @@ puts "Correct Endpoint? : #{@cw.client.endpoint} "
 @name = metric3
 @sample_metrics = AWS::CloudWatch::Metric.new(@namespace,@name)
 @sample_metrics.put_data [{:value => nodes_active}]
-
